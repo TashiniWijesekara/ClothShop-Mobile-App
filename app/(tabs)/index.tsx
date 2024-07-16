a305fa2811a52a2170d90cd0f386f0f370bd7e26
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-07-16 18:12:14 +0530

    modify

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,39 @@
-import { View ,Text , StyleSheet ,Image , Button } from "react-native";
+import { View,Dimensions ,Text , StyleSheet ,Image , Button  } from "react-native";
 import React from "react";
 import { Link, router } from 'expo-router';
 
 
 const Index = ()=> {
+  const dimensions = Dimensions.get('window');
+  const { width, height } = dimensions;
+
   return (
     // Home page or loging page 1
     <View>
+      {Object.entries(dimensions).map(([key, value]) => (
+      <View key={key} style={{ width, height, backgroundColor: 'white', display: 'flex', alignItems: 'center'}}>
+        
       <Text></Text>
       <Text style={styles.nametext}>{"\n"} ZANY Clothing </Text>
       <Image style={styles.logo} source={require('../../assets/images/cloth_logo.png')}  />
       <Text></Text>
-      <Text style={styles.welltext}>Welcome To Our Zany Online Clothing Store...</Text>
-      <Text style={styles.introtext}>Zany is a luxury fashion brand. Enter a world of fashion and sophistication 
+      <Text style={styles.welltext}>{"\n"}Welcome To Our Zany Online Clothing Store... </Text>
+      <Text style={styles.introtext}>{"\n"}Zany is a luxury fashion brand. Enter a world of fashion and sophistication 
         tailored just for you. If you are looking for ever trendy fashion, our online store
          offers a diverse collection for every wardrobe.  </Text>
          
          <Text style={styles.introtext}>{"\n"} With a commitment to quality, comfort, and the latest trends,
            we invite you to explore our virtual aisles and discover your next favorite outfit. 
-           Embrace effortless shopping and impeccable style with Zany Online Clothing store.{"\n"} {"\n"} </Text>
+           Embrace effortless shopping and impeccable style with Zany Online Clothing store. {"\n"} </Text>
          
         <Button 
-            title="Login Store"
+            title="Login Store "
             color="#F34572"
             onPress={()=>router.push('Home')} 
-            // color= "gray"
+            
             />
-
+        </View>
+      ))}
     </View>
   );
 }
@@ -35,19 +42,19 @@ export default Index;
 
 const styles = StyleSheet.create({
   nametext: {
-    fontSize: 30,
+    fontSize: 28,
     color: '#F34572',
     textAlign: 'center',
     
   },
   welltext: {
-    fontSize: 20,
-    color: 'black',
-    // textAlign: 'center',
+    fontSize: 22,
+    color: 'pink',
+    textAlign: 'center',
     
   },
   introtext: {
-    fontSize: 16,
+    fontSize: 15,
     color: 'black',
     
   },
